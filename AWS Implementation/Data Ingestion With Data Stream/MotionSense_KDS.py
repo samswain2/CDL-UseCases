059ac20fa62562b2f48b312ea07ce7e0ced6d5b4
Ruben Nakano <ruben.nakano@gmail.com>
2023-05-13 09:39:57 -0500

    Add sleep and region-name

--- AWS Implementation/Data Ingestion With Data Stream/MotionSense_KDS.py
+++ AWS Implementation/Data Ingestion With Data Stream/MotionSense_KDS.py
@@ -1,6 +1,7 @@
 import json
 import csv
 import boto3
+import time
 from typing import List
 
 def reading_csv(file_location: str) -> List:
@@ -12,7 +13,7 @@ def reading_csv(file_location: str) -> List:
 		csvReader = csv.DictReader(csvf)
 		for rows in csvReader:
 			MotionSense.append(rows)
-			if count == 1050:
+			if count == 12:
 				break
 			count+=1
 
@@ -20,28 +21,29 @@ def reading_csv(file_location: str) -> List:
 
 def create_kds(motion_sense: List) -> None:
 	# Create a kinesis client
-	client = boto3.client('kinesis')
+	region_name = 'us-east-2'
+	client = boto3.client('kinesis', region_name = region_name)
 	counter = 0
 
 	for i in motion_sense:
 
 		# Send message to Kinesis DataStream
 		response = client.put_record(
-			StreamName = "motionsense_example",
+			StreamName = "motionsense-stream",
 			Data = json.dumps(i),
 			PartitionKey = str(hash(i['tick_num']))
 		)
 
 		counter = counter + 1
-
-		# print('Message sent #' + str(counter))
+		time.sleep(5)
+		print('Message sent #' + str(counter))
 
 		if response['ResponseMetadata']['HTTPStatusCode'] != 200:
 			print('Error!')
 			print(response)
 
 if __name__ == '__main__':
-	file_location = 'data/df_all_data.csv'
+	file_location = './df_all_data.csv'
 	file_contents = reading_csv(file_location)
 
 	create_kds(file_contents)
