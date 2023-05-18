a3aeab1d12b4b34514e1cabc7c255f70a72baefc
Henry Liang <hanliang2023@u.northwestern.edu>
2023-05-18 00:26:39 -0500

    Implement websocket connection and store id to dynamoDB

---
+++ AWS Implementation/Streamlit/disconnect_websocket.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+import json
+
+dynamodb = boto3.client('dynamodb')
+CONNECTIONS_TABLE = 'websocket-connections'
+
+def lambda_handler(event, context):
+    connection_id = event['requestContext'].get("connectionId")
+    try:
+        dynamodb.delete_item(
+            TableName=CONNECTIONS_TABLE,
+            Key={'connectionId': {'S': connection_id}}
+        )
+    except Exception as e:
+        print(f"Error deleting connection ID: {e}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "Failed to disconnect"})
+        }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": "Disconnected"})
+    }
