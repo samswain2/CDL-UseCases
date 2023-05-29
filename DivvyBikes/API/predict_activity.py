bb586837c4d44bfeb62894ddf1298df5d4ee033f
rainymz <54558606+rainymz@users.noreply.github.com>
2023-05-29 10:52:31 -0500

    Add files via upload

--- DivvyBikes/API/predict_activity.py
+++ DivvyBikes/API/predict_activity.py
@@ -2,6 +2,7 @@
 import keras
 import pandas as pd
 from flask import Flask, request, jsonify
+import json
 
 from transform_data import WindowGenerator
 
@@ -9,12 +10,12 @@ from transform_data import WindowGenerator
 app = Flask(__name__)
 
 # Get model path and load model
-model_path = "../Model/DivvyBikes_LSTM.h5"
+model_path = "DivvyBikes_LSTM.h5"
 model = keras.models.load_model(model_path)
 
 # Column names
-columns = ["trips", "landmarks", "temp", "rel_humidity", "dewpoint", "apparent_temp", 
-           "precip", "rain", "snow", "cloudcover", "windspeed", 
+column_name = ["trips", "landmarks", "temp", "rel_humidity", "dewpoint", "apparent_temp", 
+           "precip", "rain", "snow", "cloudcover", "windspeed",
            "60201", "60202", "60208", "60301", "60302", "60304", 
            "60601", "60602", "60603", "60604", "60605", "60606", 
            "60607", "60608", "60609", "60610", "60611", "60612", 
@@ -45,9 +46,14 @@ def predict():
 
     # Get the incoming data from the request
     data = request.get_json()
+    #print(data)
+    data_list = json.loads(data)
+    data_dict = data_list[0]
+
+    data1 = {col: data_dict[col] for col in column_name}
 
     # Convert the data into a DataFrame and add to the buffer
-    data_buffer.append(pd.DataFrame(data, columns=columns))
+    data_buffer.append(pd.DataFrame(data1, columns=column_name, index=[0]))
 
     # If we have less than 30 data points, return a message
     if len(data_buffer) < dp_req:
@@ -58,12 +64,27 @@ def predict():
         sample = pd.concat(data_buffer[-dp_req:], ignore_index=True)
         w1 = WindowGenerator(input_width=input_width, label_width=hif, shift=hif, 
                              test_df=sample, label_columns=["trips"])
+        
+        print(w1)
 
         # Generate prediction
         prediction = model.predict(w1.test, verbose=False)
+        print(prediction)
 
         # Return the prediction as JSON
         return jsonify({"Prediction": prediction.tolist()})
+
+
+    # # data2 = {i: int(data1[i]) for i in list(data1.keys())}
+    # # print(data2)
+    # # Convert the data into a DataFrame
+    # sample = pd.DataFrame.from_dict(data1, orient='index', columns=column_name)
+
+    # # Transform the data and get the prediction
+    # prediction = model.predict(sample)
+
+    # # Return the prediction as JSON
+    # return jsonify({'prediction': str(prediction[0])})
 
 # Run the Flask app
 if __name__ == "__main__":
